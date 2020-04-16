ddb63bb5e1921a80a80816c443309e5b7d495893
molin <molinz@molins-MacBook-Pro.local>
2020-04-16 13:56:05 +0800

    fix bugs in config file

--- Storyboard-server/storyboard-socket/app.js
+++ Storyboard-server/storyboard-socket/app.js
@@ -9,7 +9,7 @@ const amqp = require("amqp-connection-manager");
 
 const { normalizePort, getMongoUrl, getRabbitmqUrl } = require("../utils");
 const { ERROR } = require("../response");
-const { SERVER_SOCKER_PORT, SERVER_NAME } = require("../config/server.config");
+const { SERVER_SOCKET_PORT, SERVER_NAME } = require("../config/server.config");
 const MONGO_CLUSTER = require("../config/mongo-cluster.config");
 const RABBITMQ_CLUSTER = require("../config/rabbitmq-cluster.config");
 const { createSocketServer, processMessage } = require("./socket");
@@ -31,14 +31,14 @@ app.use((req, res) => {
   console.log(req.url);
   return res.status(404).json({
     message: ERROR.NOT_FOUND,
-    data: req.url
+    data: req.url,
   });
 });
 
 app.use((err, req, res, next) => {
   return res.status(500).json({
     message: err.message ? err.message : ERROR.SERVER_ERROR,
-    data: req.url
+    data: req.url,
   });
 });
 
@@ -50,10 +50,10 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
     authSource: MONGO_CLUSTER.AUTH_DB,
-    auth: MONGO_CLUSTER.AUTH
+    auth: MONGO_CLUSTER.AUTH,
   })
   .then(() => console.log("connected to mongodb"))
-  .catch(err => console.log(`connect to mongodb error: ${err}`));
+  .catch((err) => console.log(`connect to mongodb error: ${err}`));
 mongoose.set("useFindAndModify", false);
 
 // 4. setup rabbitmq connection
@@ -66,12 +66,12 @@ const rabbitmqConn = amqp.connect(rabbitHost);
 rabbitmqConn.on("connect", () => {
   console.log("rabbitmq cluster connected");
 });
-rabbitmqConn.on("disconnect", err => {
+rabbitmqConn.on("disconnect", (err) => {
   console.log(`rabbitmq cluster disconnected with err: ${err}`);
 });
 const broadcastChannel = rabbitmqConn.createChannel({
   json: true,
-  setup: channel =>
+  setup: (channel) =>
     Promise.all([
       channel.assertExchange(
         RABBITMQ_CLUSTER.EXCHANGE.BROADCAST.NAME,
@@ -80,23 +80,23 @@ const broadcastChannel = rabbitmqConn.createChannel({
       ),
       channel.assertQueue("", { exclusive: true }),
       channel.bindQueue("", RABBITMQ_CLUSTER.EXCHANGE.BROADCAST.NAME, ""),
-      channel.consume("", data => {
+      channel.consume("", (data) => {
         let message = JSON.parse(data.content.toString());
         console.log(message);
         broadcastChannel.ack(data);
-      })
-    ])
+      }),
+    ]),
 });
 
 const unicastChannel = rabbitmqConn.createChannel({
   json: true,
-  setup: channel =>
+  setup: (channel) =>
     Promise.all([
       channel.assertExchange(
         RABBITMQ_CLUSTER.EXCHANGE.UNICAST.NAME,
         RABBITMQ_CLUSTER.EXCHANGE.UNICAST.TYPE,
         {
-          durable: true
+          durable: true,
         }
       ),
       channel.assertQueue(SERVER_NAME, { exclusive: true }),
@@ -105,7 +105,7 @@ const unicastChannel = rabbitmqConn.createChannel({
         RABBITMQ_CLUSTER.EXCHANGE.UNICAST.NAME,
         SERVER_NAME
       ),
-      channel.consume(SERVER_NAME, async data => {
+      channel.consume(SERVER_NAME, async (data) => {
         try {
           let message = JSON.parse(data.content.toString());
           const resp = await processMessage(message, app);
@@ -114,15 +114,15 @@ const unicastChannel = rabbitmqConn.createChannel({
         } catch (err) {
           console.log(err);
         }
-      })
-    ])
+      }),
+    ]),
 });
 
 app.locals.broadcast = broadcastChannel;
 app.locals.unicast = unicastChannel;
 
 // 5. setup server
-let port = normalizePort(process.env.PORT || SERVER_SOCKER_PORT);
+let port = normalizePort(process.env.PORT || SERVER_SOCKET_PORT);
 app.set("port", port);
 const server = http.createServer(app);
 
